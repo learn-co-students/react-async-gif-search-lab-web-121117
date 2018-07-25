62e32aa32f36865e5bb873811f123b7772822d16
maxwell <github email address>
2018-07-25 18:24:43 -0400

    added solution

---
+++ src/components/GifList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GifList = props => {
+  console.log(props);
+  return (
+    <div>
+        {props.gifs.map(gif => <img key={gif.url} src={gif.url} alt="gif"/>)}
+    </div>
+  )
+}
+
+export default GifList
