f86d9380ccc3426d391bf55cd660c0408311140d
maxwell <github email address>
2018-07-25 18:24:43 -0400

    added solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import NavBar from './NavBar';
 import GifListContainer from '../containers/GifListContainer';
-
 // the App component should render out the GifListContainer component
 
 function App() {
