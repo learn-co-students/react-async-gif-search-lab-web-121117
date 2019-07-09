4eda211a61e4dcc5023c3c251d6e6e64f75f4ce1
maxwellbenton <maxwellbenton@gmail.com>
2019-07-09 13:38:24 -0400

    merge with rebased master

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
+import GifListContainer from "../containers/GifListContainer";
+import NavBar from "./NavBar";
 
-import NavBar from './NavBar';
-import GifListContainer from '../containers/GifListContainer';
 // the App component should render out the GifListContainer component
 
-function App() {
-	return (
-		<div>
-			<NavBar />
-			<GifListContainer />
-		</div>
-	);
-}
+const App = () => {
+  return (
+    <div>
+      <NavBar color="black" title="Giphy Search" />
+      <GifListContainer />
+    </div>
+  );
+};
 
 export default App;
